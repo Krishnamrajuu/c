d9211c2e691fdb8092af7674527296e55b4d9b38
Krishnamrajuu <92290988+Krishnamrajuu@users.noreply.github.com>
2021-11-03 11:27:26 +0530

    Add files via upload

---
+++ square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+	int side,area,perimeter;
+	
+	side = 2;
+	area = side*side;
+	perimeter = 4*side;
+	
+	printf("area of the square with side 2 is %d\n", area);
+	printf("perimeter of the square with side 2 is %d", perimeter);
+}
